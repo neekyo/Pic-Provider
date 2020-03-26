32452a32f66567fad7c4689c5347925bba47864c
Nick Sherman <ogleffeckt2@yahoo.com>
2020-03-26 19:07:26 -0400

    added preview pic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../assets/unsplash.png';
 
 class SearchBar extends React.Component {
 	state = { term: '' };
@@ -11,18 +12,21 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div className="ui segment">
-				<form onSubmit={this.onFormSubmit} className="ui form">
-					<div className="field">
-						<label>Image Search</label>
-						<input
-							type="text"
-							value={this.state.term}
-							onChange={(e) => this.setState({ term: e.target.value })}
-						/>
-					</div>
-				</form>
-			</div>
+			<React.Fragment>
+				<img className="logo" src={logo} alt="logo" />
+				<div className="ui segment">
+					<form onSubmit={this.onFormSubmit} className="ui form">
+						<div className="field">
+							<label>Image Search</label>
+							<input
+								type="text"
+								value={this.state.term}
+								onChange={(e) => this.setState({ term: e.target.value })}
+							/>
+						</div>
+					</form>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
